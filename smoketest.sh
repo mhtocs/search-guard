3ef3333659e59f93e629a252e171510849f0ecf2
Hendrik Saly <hsaly@floragunn.com>
2019-11-27 14:08:14 +0000

    Merge branch 'es-6.8.5-lhtests' into 'es-6.8.5'
    Moving from circleci to official maven image for build pipeline
    
    See merge request search-guard/search-guard!2

--- smoketest.sh
+++ smoketest.sh
@@ -3,6 +3,7 @@ killall -9 java
 shopt -s extglob
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR
 cd $DIR
 
 ES_VERSION=6.8.0
@@ -11,6 +12,7 @@ NETTY_NATIVE_CLASSIFIER=non-fedora-linux-x86_64
 
 rm -rf elasticsearch-$ES_VERSION
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ES_VERSION.tar.gz
+chmod 777 elasticsearch-$ES_VERSION.tar.gz
 tar -xzf elasticsearch-$ES_VERSION.tar.gz
 rm -rf elasticsearch-$ES_VERSION.tar.gz
 #wget -O netty-tcnative-$NETTY_NATIVE_VERSION-$NETTY_NATIVE_CLASSIFIER.jar https://search.maven.org/remotecontent?filepath=io/netty/netty-tcnative/$NETTY_NATIVE_VERSION/netty-tcnative-$NETTY_NATIVE_VERSION-$NETTY_NATIVE_CLASSIFIER.jar
@@ -30,10 +32,22 @@ fi
 
 #cp netty-tcnative-$NETTY_NATIVE_VERSION-$NETTY_NATIVE_CLASSIFIER.jar elasticsearch-$ES_VERSION/plugins/search-guard-ssl/
 rm -f netty-tcnative-$NETTY_NATIVE_VERSION-$NETTY_NATIVE_CLASSIFIER.jar
+if [ "$CI" == "true" ]; then
+  echo "Adding esuser user"
+  useradd esuser
+  mkdir /home/esuser
+  chown esuser:esuser /home/esuser -R
+  chown esuser:esuser $DIR/elasticsearch-$ES_VERSION -R
+  usermod -aG sudo esuser
+fi
+
+echo "Plugin installation"
 
 chmod +x elasticsearch-$ES_VERSION/plugins/search-guard-6/tools/install_demo_configuration.sh
 ./elasticsearch-$ES_VERSION/plugins/search-guard-6/tools/install_demo_configuration.sh -y -i
-elasticsearch-$ES_VERSION/bin/elasticsearch -p es-smoketest-pid &
+
+echo "ES starting up"
+sudo -E -u esuser elasticsearch-$ES_VERSION/bin/elasticsearch -p es-smoketest-pid &
 
 while ! nc -z 127.0.0.1 9200; do
   sleep 0.1 # wait for 1/10 of the second before check again
